9482714941d3cfae9c5619bdc5d35f0c560fe282
Ian Macdonald <ian@caliban.org>
2011-08-16 20:58:20 +0200

    Revert "Test"
    This reverts commit ecd9ae3f25cb83ad2123f740f914cab1f266c614.

--- sonospy/scan.py
+++ sonospy/scan.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# Author: Mark Henkelis <mark.henkelis@tesco.net> xxxx
+# Author: Mark Henkelis <mark.henkelis@tesco.net>
 
 import os, sys
 import optparse
