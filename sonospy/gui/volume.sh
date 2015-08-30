1fce7f029f55b60117d62f05ff0ce996e4ed3fca
chowanec <chowanec@gmail.com>
2015-08-30 10:34:09 -0700

    Added volume limits per zone
    Seems to work fine in light testing.

--- sonospy/gui/volume.sh
+++ sonospy/gui/volume.sh
@@ -1,41 +1,42 @@
 #!/bin/bash
 # Daemonized loop below for use with volumed.sh
 #----------------------------------------------------------------------------
-# TO DO:	Set volume per zone thresholds.
 
 while :
 do
+	# Here to prevent race conditions
+	sleep 1
+
 	# Add zones here, separate zone names with a space.  They are case
-	# sensitive.
+	# sensitive. Set volume limits in the order of the zones listed
+	# below.  I.e. the first zone volume will apply to the first 
+	# zone name in $sonosZONE
 
 	sonosZONE=( Deck Spa )
-
-	# Check the current time.  If it is after 11pm set the max volume to 20%
-	# If it is after midnight and before 7am, set it to 0.
-
-	curTIME=`date +%k%M`
-
-	if [ "$curTIME" -gt  2300 -a "$curTIME" -lt 2359 ]
-	then
-		maxVOLUME=20
-	else
-		maxVOLUME=50
-	fi
-
-	if [ "$curTIME" -gt 0 -a  "$curTIME" -lt 700 ]
-	then
-		# Effectively mute it.
-		maxVOLUME=0
-	else
-		maxVOLUME=50
-	fi
+	zoneVOLUME=( 50 50 )
 
 	# Loop through the zones and check their volume.  Reset accordingly.
-
+	ctr=0
 	for i in "${sonosZONE[@]}"
 	do
-		sleep 1
-		stripME="VOLUME::"
+		# Get our Zone Specific Max Volume
+		maxVOLUME="${zoneVOLUME[$ctr]}"
+
+	        # Check the current time.  If it is after 11pm set the max volume to 20%
+        	# If it is after midnight and before 7am, set it to 0.
+
+	        curTIME=`date +%k%M`
+
+	        if [ "$curTIME" -gt  2300 -a "$curTIME" -lt 2359 ]
+        	then
+                	maxVOLUME=20
+	        fi
+
+        	if [ "$curTIME" -gt 0 -a  "$curTIME" -lt 700 ]
+	        then
+        	        # Effectively mute it.
+                	maxVOLUME=0
+	        fi
 
 		# If we've made it this far, we are checking now to see if the current volume is > maxVOLUME
 		# as defined above.  If it is, reset the volume.
@@ -57,5 +58,7 @@ do
 		then
 	        	curl -s http://192.168.1.110:50101/data/rendererAction?data=VOLUME::$maxVOLUME &>/dev/null
 		fi
+
+		ctr=$(( ctr + 1 ))
 	done
 done
